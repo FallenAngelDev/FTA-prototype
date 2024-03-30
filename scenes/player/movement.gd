230030df8da9956f22e0be632ac997c7753d9388
FallenAngelDev <87909641+FallenAngelDev@users.noreply.github.com>
2024-03-30 07:35:20 -0300

    adding hightmap terrain plugin

--- scenes/player/movement.gd
+++ scenes/player/movement.gd
@@ -2,10 +2,13 @@ extends CharacterBody3D
 
 @onready var camera := $CameraArm as Node3D
 @onready var anim_tree := $male_human_base/AnimationTree as AnimationTree
+@onready var body := $male_human_base
 @onready var blend_space := "parameters/BlendSpace2D/blend_position"
-var sensibility := 0.01
 
-const SPEED = 5.0
+var sensibility := 0.01
+var aceleration = 1
+
+const MAX_SPEED = 7
 const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -28,13 +31,14 @@ func _physics_process(delta: float) -> void:
 	#var input_sides := Input.get_axis("right","left")
 	var direction := (camera.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		anim_tree[blend_space] = lerp(anim_tree[blend_space], input_dir, 0.1)
-		$male_human_base.rotation.y = lerp($male_human_base.rotation.y, camera.rotation.y, 0.1)
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		var direction_rad = Vector2(direction.z, direction.x).angle()
+		anim_tree[blend_space] = lerp(anim_tree[blend_space], input_dir, 0.05)
+		body.rotation.y = lerp_angle(body.rotation.y,direction_rad,0.1)
+		velocity.x = lerp(velocity.x, direction.x * MAX_SPEED, delta * aceleration)
+		velocity.z = lerp(velocity.z, direction.z * MAX_SPEED, delta * aceleration)
 	else:
 		anim_tree[blend_space] = lerp(anim_tree[blend_space], Vector2.ZERO , 0.1)
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, MAX_SPEED)
+		velocity.z = move_toward(velocity.z, 0, MAX_SPEED)
 
 	move_and_slide()
